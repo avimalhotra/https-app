9b2817b52a9f314408fddfa299c515cd59a9db13
Avinash Malhotra <avimalhotra505@gmail.com>
2020-06-06 22:30:34 +0530

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,40 @@
+const express=require('express');
+var app=express();
+var bodyParser=require("body-parser");
+var https=require('https');
+var http=require("http")
+var fs=require('fs');
+var options = {
+    key: fs.readFileSync('conf/private-key.pem'), 
+    cert: fs.readFileSync('conf/public-cert.pem'),
+    //ca:[fs.readFileSync('conf/')]
+    };
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/',(req,res)=>{
+    res.send("hi");
+})
+
+var data=["sun","mon","tues","wed","thurs","fri","sat"];
+
+app.get("/api",(req,res)=>{
+    //res.header('Access-Control-Allow-Origin',"*")
+   
+    //console.log(res.json(req.body));
+     return res.send(data)
+})
+
+
+var httpsServer=https.createServer( options, app);
+var httpServer=http.createServer(app);
+
+
+httpServer.listen(3000,()=>{
+    console.log(`http://127.0.0.1:3000`)
+})
+
+httpsServer.listen(3030,()=>{
+    console.log(`https://127.0.0.1:3030`)
+})
